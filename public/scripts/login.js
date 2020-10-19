de009db23d47a21b9a103534a54eaf61478f7e6c
ekprojects71 <60908597+ekprojects71@users.noreply.github.com>
2020-10-19 01:49:43 -0500

    reverting to an old version

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,8 +1,23 @@
 const loginForm = document.querySelector(".login");
 const loginErrorMsg = document.querySelector(".login-error-message span");
 const loginErrorCont = document.querySelector(".login-error-message");
+const demoButtons = document.querySelector(".demo-list");
 
 
+//demo login function
+const demoLogin = async(credentials) => {
+    const response = await fetch(`/auth/login/demo/${credentials}`, { method: "POST" });
+    if(response.status === 200) {
+        loginErrorCont.classList.add("d-none");
+        location.reload();
+    }
+    else {
+        loginErrorCont.classList.remove("d-none");
+        loginErrorMsg.innerText = "Unable to log in. Please try again later.";
+    }
+}
+
+//event listeners for login
 loginForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -19,4 +34,18 @@ loginForm.addEventListener("submit", (e) => {
                 loginErrorMsg.innerText = "Invalid email or password. Check your spelling and try again.";
             }
         });
+});
+
+demoButtons.addEventListener("click", e => {
+    e.preventDefault();
+
+    if(e.target.classList.contains("admin-demo")) {
+        demoLogin(1);
+    }
+    else if(e.target.classList.contains("dev-demo")) {
+        demoLogin(2);
+    }
+    else if(e.target.classList.contains("user-demo")) {
+        demoLogin(3);
+    }
 });
